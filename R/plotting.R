dd18a7f64b910c20ceba5b50eeee9ca7c31b3510
Smith <r03es23@abdn.ac.uk>
2023-12-15 18:18:21 +0000

    Implementing speedyseq for glomming.

--- R/plotting.R
+++ R/plotting.R
@@ -94,7 +94,7 @@ get_big_colours <- function(){
 #' @examples
 getgloms <- function(ps,ranks= c("Phylum" ,"Class","Order","Family","Genus")){
   ps@tax_table@.Data[is.na(ps@tax_table@.Data)] <- "Unassigned"
-  gloms <- lapply(ranks, function(x) tax_glom(ps,taxrank=x,NArm=FALSE)) #agglomerate counts per taxon at each of the specied ranks
+  gloms <- lapply(ranks, function(x) speedyseq::tax_glom(ps,taxrank=x,NArm=FALSE)) #agglomerate counts per taxon at each of the specied ranks
   names(gloms) <- ranks
   return(gloms)
 }
@@ -126,7 +126,7 @@ plot_taxa_abundance <- function(ps,rank,xsep, wrap = NULL, n=20, colno = NULL,by
   if(!length(unique(as.data.frame(ps@tax_table@.Data)[,rank])) == length(unique(rownames((as.data.frame(ps@tax_table@.Data)))))){
     print("Agglomerating at specified rank...")
     ps@tax_table@.Data[is.na(ps@tax_table@.Data)] <- "Unassigned"
-    glom <-tax_glom(ps, taxrank = rank, NArm = FALSE)
+    glom <-speedyseq::tax_glom(ps, taxrank = rank, NArm = FALSE)
     print("Done.")
   }else{
     print("Using provided agglomerated object.")
